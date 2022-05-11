e5f33d334193b4f8321874e06ada9eaca8bf8e20
Umair Sadiq <59564704+umairsadiq88@users.noreply.github.com>
2022-05-11 19:17:59 +0500

    Hero section completed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,18 +6,19 @@ import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
   <>
-    <Section row nopadding>
-      <LeftSection>
-        <SectionTitle main center>
-          Welcome To <br />
-          My Personal Portfolio
-        </SectionTitle>
-        <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
-        </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
-      </LeftSection>
-    </Section>
+
+<Section row nopadding>
+  <LeftSection>
+    <SectionTitle main center>
+      Welcome to My Personal Portfolio
+    </SectionTitle>
+    <SectionText>
+    My development Services 
+    </SectionText>
+    <Button onClick={props.handleClick}>Learn More</Button>
+  </LeftSection>
+</Section>
+
   </>
 );
 
