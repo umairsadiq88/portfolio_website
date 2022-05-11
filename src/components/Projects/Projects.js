e7514fd1dbb2510c5afb6b71062835d7db6c5c11
Umair Sadiq <59564704+umairsadiq88@users.noreply.github.com>
2022-05-11 20:18:00 +0500

    project secion

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,37 +4,52 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+
+
 const Projects = () => (
+  
   <Section nopadding id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {projects.map((p, i) => {
-        return (
-          <BlogCard key={i}>
-          <Img src={p.image} />
-            <TitleContent>
-              <HeaderThree title>{p.title}</HeaderThree>
-              <Hr />
-            </TitleContent>
-            <CardInfo className="card-info">{p.description}</CardInfo>
-            <div>
-              <TitleContent>Stack</TitleContent>
-              <TagList>
-                {p.tags.map((t, i) => {
-                  return <Tag key={i}>{t}</Tag>;
-                })}
-              </TagList>
-            </div>
-            <UtilityList>
-              <ExternalLinks href={p.visit}>Code</ExternalLinks>
-              <ExternalLinks href={p.source}>Source</ExternalLinks>
-            </UtilityList>
-          </BlogCard>
-        );
-      })}
+      {projects.map(({id, image, title, description,tags,source,visit}) => (
+<BlogCard key={id}>
+<img src={image} />
+<TitleContent>
+  <HeaderThree title>
+    {title}
+  </HeaderThree>
+  <Hr />
+  </TitleContent>
+
+<CardInfo>
+  {description}
+</CardInfo>
+<div>
+  <TitleContent>
+    Stack
+  </TitleContent>
+</div>
+<TagList>
+{p.tags.map((t, i) => {
+                 return <Tag key={i}>{t}</Tag>;
+               })}
+             </TagList>
+</BlogCard>
+   )      ) }
     </GridContainer>
   </Section>
+  
+  //           </div>
+  //           <UtilityList>
+  //             <ExternalLinks href={p.visit}>Code</ExternalLinks>
+  //             <ExternalLinks href={p.source}>Source</ExternalLinks>
+  //           </UtilityList>
+  //         </BlogCard>
+  //       );
+  //     })}
+  //   </GridContainer>
+  // </Section>
 );
 
 export default Projects;
