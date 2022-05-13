c22569db9b1d8ce7eb7ff4dc4bb36ed380e194a0
Umair Sadiq <59564704+umairsadiq88@users.noreply.github.com>
2022-05-13 17:44:31 +0500

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,7 +16,7 @@ const Header = () => (
             color: "white",
             marginBottom: "20px"
           }}>
-          <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+          <DiCssdeck size="3rem" /> <Span>Umair Sadiq</Span>
         </a>
       </Link>
     </Div1>
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiReact, DiZend, DiDocker, DiCloud9, DiPhotoshop, DiIllustrator} from 'react-icons/di';
 
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
@@ -34,13 +34,36 @@ const Technologies = () => (
           </ListParagraph>
         </ListContainer>
       </ListItem>
-      <ListItem>
 
+      <ListItem>
         <DiZend size="3rem" />
         <ListContainer>
           <ListTitle>UX-UI Designer</ListTitle>
           <ListParagraph>
             Experience with Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+      <ListItem>
+        <DiCloud9 size="3rem" />
+        <ListContainer>
+          <ListTitle>DevOps</ListTitle>
+          <ListParagraph>
+            Experience with Dockers, Kubernetes
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      
+
+      <ListItem>
+        <DiIllustrator size="3rem" />
+      
+
+        <ListContainer>
+          <ListTitle>Graphics Designer</ListTitle>
+          <ListParagraph>
+            Experience with Adobe Illustrator,   Adobe Photoshop, Corel Draw
           </ListParagraph>
         </ListContainer>
       </ListItem>
